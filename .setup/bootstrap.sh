6582a8540558a1b8c475d55d6e3a35dba7241b44
Dmitriy <ds@mobioinc.com>
2016-08-12 16:15:02 +0300

    Remove useless code in bootstrap.sh

--- .setup/bootstrap.sh
+++ .setup/bootstrap.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="/var/www/.setup/scripts/"
-SCRIPT=""
 
-if [ "$SCRIPT" != "" ]; then
-    echo "[MOBIO SH Scripts]: Run one script: ${DIR}${SCRIPT}"
-    echo "***"
-    echo "[MOBIO SH Scripts]: ${SCRIPT} [START]"
-    . ${DIR}${SCRIPT}
-    echo "[MOBIO SH Scripts]: ${SCRIPT} [START]"
-else
-    echo "[MOBIO SH Scripts]: Iterate all scripts"
-    for file in ${DIR}*; do
-      echo "***"
-      echo "[MOBIO SH Scripts]: ${file} [START]"
-      . ${file}
-      echo "[MOBIO SH Scripts]: ${file} [END]"
-    done
-fi
+for file in ${DIR}*; do
+  echo "***"
+  echo "[MOBIO SH Scripts]: ${file} [START]"
+  . ${file}
+  echo "[MOBIO SH Scripts]: ${file} [END]"
+done
